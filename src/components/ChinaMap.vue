35e4246c4febfd1b3aea6e1918bcc419ba9a4526
rocklea <rocklea@163.com>
2025-04-15 07:43:04 +0800

    更新：1.修改地图组件配置 2.更新纹理图片 3.添加地图数据

--- src/components/ChinaMap.vue
+++ src/components/ChinaMap.vue
@@ -6,7 +6,8 @@
     <div v-if="!loading && !error" class="camera-info">
       <h3>当前区域: {{ currentArea }}</h3>
       <div class="button-group">
-        <button v-if="currentArea !== '湖南省'" @click="backToProvince">返回湖南省</button>
+        <button v-if="currentArea !== '中国'" @click="backToChina">返回全国</button>
+        <button v-if="currentArea !== '湖南省' && currentArea !== '中国'" @click="backToProvince">返回湖南省</button>
         <button @click="resetCamera">复位视角</button>
       </div>
       <p>距离: {{ cameraDistance }}</p>
@@ -22,9 +23,11 @@ import * as echarts from 'echarts';
 import 'echarts-gl';
 import { onMounted, onUnmounted, ref, nextTick } from 'vue';
 import hunanMapData from '../map/430000.json';
-import HNImg from '../assets/textures/wenli.png';
+import chinaMapData from '../map/100000.json';
+import HNImg from '../assets/textures/fn.png';
+import CNImg from '../assets/textures/zg.png';
 
-// 使用数组形式存储城市坐标数据
+// 使用数组形式存储湖南市州坐标数据
 const cityCoordinates = [
   ['长沙市', 112.982279, 28.19409, '430100'],
   ['株洲市', 113.151737, 27.835806, '430200'],
@@ -42,7 +45,60 @@ const cityCoordinates = [
   ['湘西土家族苗族自治州', 109.739735, 28.314296, '433100']
 ];
 
-// 将数组转换为对象形式（用于保持其他功能兼容）
+// 使用数组形式存储全国所有省份、自治区、直辖市坐标数据
+const provinceCoordinates = [
+  // 华北地区
+  ['北京市', 116.405285, 39.904989, '110000'],
+  ['天津市', 117.190182, 39.125596, '120000'],
+  ['河北省', 114.502461, 38.045474, '130000'],
+  ['山西省', 112.549248, 37.857014, '140000'],
+  ['内蒙古自治区', 111.670801, 40.818311, '150000'],
+
+  // 东北地区
+  ['辽宁省', 123.429096, 41.796767, '210000'],
+  ['吉林省', 125.324501, 43.886841, '220000'],
+  ['黑龙江省', 126.642464, 45.756967, '230000'],
+
+  // 华东地区
+  ['上海市', 121.472644, 31.231706, '310000'],
+  ['江苏省', 118.767413, 32.041544, '320000'],
+  ['浙江省', 120.153576, 30.287459, '330000'],
+  ['安徽省', 117.283042, 31.86119, '340000'],
+  ['福建省', 119.306239, 26.075302, '350000'],
+  ['江西省', 115.892151, 28.676493, '360000'],
+  ['山东省', 117.000923, 36.675807, '370000'],
+
+  // 华中地区
+  ['河南省', 113.665412, 34.757975, '410000'],
+  ['湖北省', 114.298572, 30.584355, '420000'],
+  ['湖南省', 112.982279, 28.19409, '430000'],
+
+  // 华南地区
+  ['广东省', 113.280637, 23.125178, '440000'],
+  ['广西壮族自治区', 108.320004, 22.82402, '450000'],
+  ['海南省', 110.33119, 20.031971, '460000'],
+
+  // 西南地区
+  ['重庆市', 106.504962, 29.533155, '500000'],
+  ['四川省', 104.065735, 30.659462, '510000'],
+  ['贵州省', 106.713478, 26.578343, '520000'],
+  ['云南省', 102.712251, 25.040609, '530000'],
+  ['西藏自治区', 91.132212, 29.660361, '540000'],
+
+  // 西北地区
+  ['陕西省', 108.948024, 34.263161, '610000'],
+  ['甘肃省', 103.823557, 36.058039, '620000'],
+  ['青海省', 101.778916, 36.623178, '630000'],
+  ['宁夏回族自治区', 106.278179, 38.46637, '640000'],
+  ['新疆维吾尔自治区', 87.617733, 43.792818, '650000'],
+
+  // 特别行政区
+  ['香港特别行政区', 114.173355, 22.320048, '810000'],
+  ['澳门特别行政区', 113.54909, 22.198951, '820000'],
+  ['台湾省', 121.509062, 25.044332, '710000']
+];
+
+// 将数组转换为对象形式
 const HUNAN_CITIES = cityCoordinates.reduce((acc, [name, lng, lat, code]) => {
   acc[name] = {
     center: [lng, lat],
@@ -52,37 +108,70 @@ const HUNAN_CITIES = cityCoordinates.reduce((acc, [name, lng, lat, code]) => {
 }, {});
 
 export default {
-  name: 'HunanMap',
+  name: 'ChinaMap',
   setup() {
     const mapContainer = ref(null);
     let chart = null;
     const loading = ref(true);
     const error = ref('');
-    const currentArea = ref('湖南省');
+    const currentArea = ref('中国');
     
     // 默认视角参数
     const DEFAULT_CAMERA = {
-      distance: 160,
-      alpha: 70,
-      beta: 0,
-      center: [0.735583, 23.998643, 11.526896]
+      china: {
+        distance: 200,
+        alpha: 70,
+        beta: 0,
+        center: [2.786219, 29.710574, 6.20325]
+      },
+      province: {
+        distance: 160,
+        alpha: 70,
+        beta: 0,
+        center: [0.735583, 23.998643, 11.526896]
+      }
     };
     
-    // 修改默认视角参数
-    const cameraDistance = ref(DEFAULT_CAMERA.distance);
-    const cameraAlpha = ref(DEFAULT_CAMERA.alpha);
-    const cameraBeta = ref(DEFAULT_CAMERA.beta);
-    const cameraCenter = ref([...DEFAULT_CAMERA.center]);
+    // 地图样式配置
+    const mapConfig = {
+      boxHeight: 8,
+      regionHeight: 6,
+      itemStyle: {
+        color: '#fff',
+        opacity: 0.8,
+        borderWidth: 1,
+        borderColor: '#fff'
+      }
+    };
 
-    // 修改生成随机数据的函数
-    const generateRandomData = (mapData, isProvince = true) => {
-      if (isProvince) {
+    // 柱状图配置
+    const barConfig = {
+      barSize: 2,
+      minHeight: 10,
+      itemStyle: {
+        color: '#FFD700',
+        opacity: 0.6
+      }
+    };
+    
+    const cameraDistance = ref(DEFAULT_CAMERA.china.distance);
+    const cameraAlpha = ref(DEFAULT_CAMERA.china.alpha);
+    const cameraBeta = ref(DEFAULT_CAMERA.china.beta);
+    const cameraCenter = ref([...DEFAULT_CAMERA.china.center]);
+
+    // 生成随机数据函数修改
+    const generateRandomData = (mapData, level = 'country') => {
+      if (level === 'country') {
+        return provinceCoordinates.map(([name, lng, lat]) => ({
+          name,
+          value: [lng, lat, Math.random() * 100]
+        }));
+      } else if (level === 'province') {
         return cityCoordinates.map(([name, lng, lat]) => ({
           name,
           value: [lng, lat, Math.random() * 100]
         }));
       } else {
-        // 处理市州级别的数据
         if (!mapData || !mapData.features) return [];
         return mapData.features.map(feature => ({
           name: feature.properties.name,
@@ -118,14 +207,12 @@ export default {
     const addRealTimeListener = () => {
       if (!chart) return;
       
-      // 监听鼠标事件
       ['mousedown', 'mouseup', 'mousemove', 'mousewheel'].forEach(eventName => {
         chart.getZr().on(eventName, () => {
           requestAnimationFrame(updateCameraParams);
         });
       });
       
-      // 监听触摸事件
       ['touchstart', 'touchmove', 'touchend'].forEach(eventName => {
         chart.getZr().on(eventName, () => {
           requestAnimationFrame(updateCameraParams);
@@ -133,12 +220,22 @@ export default {
       });
     };
 
+    // 返回全国地图
+    const backToChina = async () => {
+      currentArea.value = '中国';
+      try {
+        await initChart('中国', chinaMapData, 'country');
+      } catch (err) {
+        error.value = err.message;
+        console.error(err);
+      }
+    };
+
     // 返回湖南省地图
     const backToProvince = async () => {
       currentArea.value = '湖南省';
       try {
-        // 使用本地地图数据
-        await initChart('湖南', hunanMapData);
+        await initChart('湖南', hunanMapData, 'province');
       } catch (err) {
         error.value = err.message;
         console.error(err);
@@ -149,15 +246,19 @@ export default {
     const resetCamera = () => {
       if (!chart) return;
       
+      const cameraConfig = currentArea.value === '中国' 
+        ? DEFAULT_CAMERA.china 
+        : DEFAULT_CAMERA.province;
+      
       const option = {
         geo3D: {
           viewControl: {
             projection: 'perspective',
-            alpha: DEFAULT_CAMERA.alpha,
-            beta: DEFAULT_CAMERA.beta,
-            distance: DEFAULT_CAMERA.distance,
+            alpha: cameraConfig.alpha,
+            beta: cameraConfig.beta,
+            distance: cameraConfig.distance,
             minDistance: 40,
-            maxDistance: 200,
+            maxDistance: currentArea.value === '中国' ? 400 : 200,
             minAlpha: 10,
             maxAlpha: 90,
             minBeta: -180,
@@ -165,18 +266,17 @@ export default {
             animation: true,
             animationDurationUpdate: 1000,
             orthographicSize: 100,
-            center: DEFAULT_CAMERA.center
+            center: cameraConfig.center
           }
         }
       };
       
       chart.setOption(option);
       
-      // 更新显示的参数
-      cameraDistance.value = DEFAULT_CAMERA.distance;
-      cameraAlpha.value = DEFAULT_CAMERA.alpha;
-      cameraBeta.value = DEFAULT_CAMERA.beta;
-      cameraCenter.value = [...DEFAULT_CAMERA.center];
+      cameraDistance.value = cameraConfig.distance;
+      cameraAlpha.value = cameraConfig.alpha;
+      cameraBeta.value = cameraConfig.beta;
+      cameraCenter.value = [...cameraConfig.center];
     };
 
     // 计算地图边界和尺寸比例
@@ -238,7 +338,7 @@ export default {
     };
 
     // 初始化地图
-    const initChart = async (mapName = '湖南', mapData = null) => {
+    const initChart = async (mapName, mapData, level = 'country') => {
       try {
         loading.value = true;
         error.value = '';
@@ -255,14 +355,12 @@ export default {
         
         chart = echarts.init(mapContainer.value);
         
-        // 注册地图数据
         if (mapData) {
           echarts.registerMap(mapName, mapData);
         }
         
-        const isProvince = mapName === '湖南';
-        // 计算地图尺寸
         const mapSize = calculateMapBounds(mapData);
+        const cameraConfig = level === 'country' ? DEFAULT_CAMERA.china : DEFAULT_CAMERA.province;
         
         const option = {
           backgroundColor: '#012248',
@@ -278,14 +376,14 @@ export default {
             map: mapName,
             shading: 'realistic',
             realisticMaterial: {
-              detailTexture: HNImg,  // 使用地形纹理
-              textureTiling: 1  // 纹理平铺次数 [x, y
+              detailTexture: level === 'country' ? CNImg : HNImg,
+              textureTiling: 1
             },
             environment: '#012248',
-            boxWidth: mapSize.width,
-            boxHeight: 8,  // 统一高度为8
-            boxDepth: mapSize.depth,
-            regionHeight: 6,  // 统一区域高度为6
+            boxWidth: level === 'country' ? 200 : mapSize.width,
+            boxHeight: mapConfig.boxHeight,
+            boxDepth: level === 'country' ? 160 : mapSize.depth,
+            regionHeight: mapConfig.regionHeight,
             groundPlane: {
               show: false
             },
@@ -301,11 +399,11 @@ export default {
             },
             viewControl: {
               projection: 'perspective',
-              alpha: DEFAULT_CAMERA.alpha,
-              beta: DEFAULT_CAMERA.beta,
-              distance: DEFAULT_CAMERA.distance,
+              alpha: cameraConfig.alpha,
+              beta: cameraConfig.beta,
+              distance: cameraConfig.distance,
               minDistance: 40,
-              maxDistance: 200,
+              maxDistance: level === 'country' ? 400 : 200,
               minAlpha: 10,
               maxAlpha: 90,
               minBeta: -180,
@@ -313,47 +411,24 @@ export default {
               animation: true,
               animationDurationUpdate: 1000,
               orthographicSize: 100,
-              center: DEFAULT_CAMERA.center
+              center: cameraConfig.center
             },
-            itemStyle: {
-              color: '#00BBFF',
-              opacity: 0.8,
-              borderWidth: 1,
-              borderColor: '#fff'
-            },
+            itemStyle: mapConfig.itemStyle,
             emphasis: {
               itemStyle: {
                 color: '#00EAFF',
                 opacity: 1
               }
-            },
-            regions: isProvince 
-              ? Object.keys(HUNAN_CITIES).map(name => ({
-                  name,
-                  itemStyle: {
-                    color: '#4169E1',
-                    opacity: 0.8
-                  }
-                })) 
-              : mapData.features.map(feature => ({
-                  name: feature.properties.name,
-                  itemStyle: {
-                    color: '#4169E1',
-                    opacity: 0.8
-                  }
-                }))
+            }
           },
           series: [{
             type: 'bar3D',
             coordinateSystem: 'geo3D',
-            data: generateRandomData(mapData, isProvince),
-            barSize: 2,
-            minHeight: 10,
+            data: generateRandomData(mapData, level),
+            barSize: level === 'country' ? barConfig.barSize * 1.2 : barConfig.barSize,
+            minHeight: barConfig.minHeight,
             silent: false,
-            itemStyle: {
-              color: '#FFD700',
-              opacity: 0.6,
-            },
+            itemStyle: barConfig.itemStyle,
             emphasis: {
               itemStyle: {
                 color: '#00FF00',
@@ -363,12 +438,12 @@ export default {
             label: {
               show: true,
               formatter: function(params) {
-                return Math.round(params.value[2]);  // 只显示数值
+                return Math.round(params.value[2]);
               },
               position: 'top',
               distance: 2,
               textStyle: {
-                fontSize: 20,
+                fontSize: level === 'country' ? 16 : 20,
                 fontWeight: '600',
                 color: '#000000',
                 fontFamily: 'TCloud, Arial, sans-serif',
@@ -382,7 +457,7 @@ export default {
           }, {
             type: 'scatter3D',
             coordinateSystem: 'geo3D',
-            data: generateRandomData(mapData, isProvince).map(item => ({
+            data: generateRandomData(mapData, level).map(item => ({
               name: item.name,
               value: [item.value[0], item.value[1], 0]
             })),
@@ -395,7 +470,7 @@ export default {
               position: 'left',
               distance: 10,
               textStyle: {
-                fontSize: 12,
+                fontSize: level === 'country' ? 14 : 12,
                 fontWeight: 'bold',
                 color: '#ffffff',
                 padding: [2, 4],
@@ -411,19 +486,27 @@ export default {
         chart.setOption(option);
         loading.value = false;
         
-        // 添加实时监听
         addRealTimeListener();
         
-        // 添加点击事件
         chart.on('click', async params => {
-          if (currentArea.value === '湖南省' && params.name in HUNAN_CITIES) {
+          if (currentArea.value === '中国') {
+            // 检查是否点击了省份
+            const clickedProvince = provinceCoordinates.find(([name]) => name === params.name);
+            if (clickedProvince) {
+              if (params.name === '湖南省') {
+                await backToProvince();
+              } else {
+                // 可以添加提示，表明目前只支持查看湖南省
+                error.value = '目前只支持查看湖南省的详细信息';
+              }
+            }
+          } else if (currentArea.value === '湖南省' && params.name in HUNAN_CITIES) {
             try {
               const cityCode = HUNAN_CITIES[params.name].code;
-              // 加载本地市州地图数据
               try {
                 const cityMapData = await import(`../map/${cityCode}.json`);
                 currentArea.value = params.name;
-                await initChart(params.name, cityMapData.default);
+                await initChart(params.name, cityMapData.default, 'city');
               } catch (importError) {
                 console.error(`未找到${params.name}地图数据文件: ../map/${cityCode}.json`);
                 error.value = `未找到${params.name}地图数据文件`;
@@ -435,7 +518,6 @@ export default {
           }
         });
         
-        // 添加视角变化事件监听
         chart.on('georoam', () => {
           requestAnimationFrame(updateCameraParams);
         });
@@ -448,10 +530,8 @@ export default {
     };
 
     onMounted(async () => {
-      // 初始加载湖南省地图
-      await backToProvince();
+      await backToChina();
       
-      // 监听窗口大小变化
       const handleResize = () => {
         if (chart) {
           chart.resize();
@@ -462,7 +542,6 @@ export default {
       window.addEventListener('resize', handleResize);
       
       onUnmounted(() => {
-        // 清理事件监听
         if (chart) {
           const zr = chart.getZr();
           ['mousedown', 'mouseup', 'mousemove', 'mousewheel', 'touchstart', 'touchmove', 'touchend'].forEach(eventName => {
@@ -484,6 +563,7 @@ export default {
       cameraCenter,
       loading,
       error,
+      backToChina,
       backToProvince,
       resetCamera
     };
@@ -524,36 +604,6 @@ export default {
   font-size: 14px;
 }
 
-.camera-info button {
-  margin: 10px 0;
-  padding: 5px 10px;
-  background: #1E90FF;
-  border: none;
-  border-radius: 3px;
-  color: white;
-  cursor: pointer;
-}
-
-.camera-info button:hover {
-  background: #4169E1;
-}
-
-.loading, .error {
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  padding: 20px;
-  background: rgba(1, 34, 72, 0.8);
-  color: white;
-  border-radius: 5px;
-  z-index: 1000;
-}
-
-.error {
-  color: #ff6b6b;
-}
-
 .button-group {
   display: flex;
   gap: 10px;
@@ -572,5 +622,21 @@ export default {
 
 .button-group button:hover {
   background: #4169E1;
+}
+
+.loading, .error {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  padding: 20px;
+  background: rgba(1, 34, 72, 0.8);
+  color: white;
+  border-radius: 5px;
+  z-index: 1000;
+}
+
+.error {
+  color: #ff6b6b;
 }
 </style> 
